1f02fc57fb30cf4cb1e6c82ef74e46c4c20bcacd
spending-seto <seto@spending.opendata-tokai.jp>
2014-02-03 15:16:04 +0900

    Update setting.js

--- js/setting.js
+++ js/setting.js
@@ -10,8 +10,8 @@ Taxes.taxRate = 0.06; // 住民税率
 
 var OpenSpending = OpenSpending || {};
 
-OpenSpending.identifier = 'yokohama_budget';
-OpenSpending.year = '2012';
+OpenSpending.identifier = 'seto_kessan_2011';
+OpenSpending.year = '2011';
 
 OpenSpending.Styles = OpenSpending.Styles || {};
 
